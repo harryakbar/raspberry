f0126731e31834ac302ca0855d6be3fb81de1466
Harry Akbar Ali Munir <harryakbaram@gmail.com>
2025-01-18 14:22:41 +0800

    update

--- audio/your_audio_app.py
+++ audio/your_audio_app.py
@@ -1,7 +1,22 @@
 import pygame
 import time
 
-def play_audio(file_path):
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+BOT_TOKEN = "7607085823:AAH-lo6Dm9JfXZGfwh7NJGyFCZihG1KEiZ0"
+
+def start(update, context):
+    update.message.reply_text("Hello! Use /test_speaker to test the adzan sound.")
+
+def test_speaker(update, context):
+    # Simulate speaker testing
+    update.message.reply_text("Speaker test initiated!")
+    play_audio()
+
+def play_audio():
+    file_path = "./bell.wav"
+
     # Initialize the mixer module
     pygame.mixer.init()
 
@@ -16,11 +31,16 @@ def play_audio(file_path):
     while pygame.mixer.music.get_busy():
         time.sleep(1)
 
-if __name__ == "__main__":
-    # Path to the audio file you want to play
-    audio_file = "./adzan.mp3"
+def main():
+    updater = Updater(BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("bell", test_speaker))
+    updater.start_polling()
+    updater.idle()
 
+if __name__ == "__main__":
     try:
-        play_audio(audio_file)
+        main()
     except Exception as e:
         print(f"Error: {e}")
